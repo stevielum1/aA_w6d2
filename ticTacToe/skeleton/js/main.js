b4b5a35af54b7f385229076adaab928e66bd602b
Steven Lum <stevielum1@gmail.com>
2018-07-24 17:26:11 -0700

    Complete Towers of Hanoi

--- ticTacToe/skeleton/js/main.js
+++ ticTacToe/skeleton/js/main.js
@@ -1,6 +1,7 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");
+const Game = require("../../solution/game.js");
 
 $( () => {
-  // Your code here
+  const game = new Game();
+  const view = new View(game, $('figure.ttt'));
 });
--- ticTacToe/skeleton/js/ttt-view.js
+++ ticTacToe/skeleton/js/ttt-view.js
@@ -1,11 +1,71 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.setupBoard();
+    this.bindEvents();
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    const ul = $('ul');
+    
+    ul.on("click", "li", (e) => {
+      const square = $(e.currentTarget);
+      square.addClass("white");
+      this.makeMove(square);
+    });
+    
+    ul.off("click", "hover");
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    try {
+      $square.text(this.game.currentPlayer);
+      this.game.playMove($square.data("pos"));
+      if (this.game.board.isOver()) {
+        const $figure = $('figure');
+        $figure.append(`<h2>You win, ${this.game.winner()}!</h2>`);
+        $("*").off();
+        
+        const $squares = $("li");
+        $squares.each((idx, el) => {
+          const $target = $(el);
+          console.log($target.text());
+          if ($target.text() === this.game.winner()) {
+            $target.addClass("winner");
+          } else {
+            $target.addClass("loser");
+          }
+        });
+        
+      } else {
+        
+      }
+    } catch (err) {
+      alert("Invalid move! Try again.");
+    }
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const $ul = $("<ul></ul>");
+    const $figure = $('figure.ttt');
+    
+    for(let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const $li = $("<li></li>");
+        $li.data("pos", [i, j]);
+        $ul.append($li);
+      }
+    }
+
+    $figure.append($ul);
+    
+    $ul.on("mouseenter", "li", e => {
+      $(e.currentTarget).addClass("yellow");
+    });
+    $ul.on("mouseleave", "li", e => {
+      $(e.currentTarget).removeClass("yellow");
+    });
+  }
 }
 
 module.exports = View;
